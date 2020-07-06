75c6d3d36331d901a41bc3b37d38e096d9089f32
mvdbeek <m.vandenbeek@gmail.com>
2020-07-06 12:57:46 +0200

    Reference files relative to script

--- .scripts/setup_cvmfs.sh
+++ .scripts/setup_cvmfs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 
+cd "${BASH_SOURCE%/*}/" || exit
+
 sudo apt install lsb-release
 wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
 sudo dpkg -i cvmfs-release-latest_all.deb
@@ -11,5 +13,4 @@ sudo cvmfs_config setup
 sudo cp default.local  /etc/cvmfs/default.local
 sudo cp galaxyproject.org.conf  /etc/cvmfs/domain.d/galaxyproject.org.conf
 sudo cp ./*.galaxyproject.org.pub /etc/cvmfs/keys/
-sudo mkdir /cvmfs
 sudo mount -t cvmfs main.galaxyproject.org /cvmfs/main.galaxyproject.org
