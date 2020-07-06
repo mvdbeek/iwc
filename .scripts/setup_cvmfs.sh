79b8b0381492c7be90f2ce0ab22d03612c5e86bc
mvdbeek <m.vandenbeek@gmail.com>
2020-07-06 12:51:12 +0200

    Permission fixes

--- .scripts/setup_cvmfs.sh
+++ .scripts/setup_cvmfs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -x
 
-apt install lsb-release
+sudo apt install lsb-release
 wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
-dpkg -i cvmfs-release-latest_all.deb
-apt-get update
-apt install cvmfs cvmfs-config
-cvmfs_config setup
+sudo dpkg -i cvmfs-release-latest_all.deb
+sudo apt-get update
+sudo apt install cvmfs cvmfs-config
+sudo cvmfs_config setup
 
-cp default.local  /etc/cvmfs/default.local
-cp galaxyproject.org.conf  /etc/cvmfs/domain.d/galaxyproject.org.conf
-cp ./*.galaxyproject.org.pub /etc/cvmfs/keys/
-mkdir /cvmfs
-mount -t cvmfs main.galaxyproject.org /cvmfs/main.galaxyproject.org
+sudo cp default.local  /etc/cvmfs/default.local
+sudo cp galaxyproject.org.conf  /etc/cvmfs/domain.d/galaxyproject.org.conf
+sudo cp ./*.galaxyproject.org.pub /etc/cvmfs/keys/
+sudo mkdir /cvmfs
+sudo mount -t cvmfs main.galaxyproject.org /cvmfs/main.galaxyproject.org
