5a3702b52c10a93b552bc1fd287a3770fcb62ca1
mvdbeek <m.vandenbeek@gmail.com>
2020-10-15 16:13:34 +0200

    Fix cvmfs setup and actually use $GALAXY_RELEASE

--- .scripts/setup_cvmfs.sh
+++ .scripts/setup_cvmfs.sh
@@ -8,13 +8,11 @@ wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
 sudo dpkg -i cvmfs-release-latest_all.deb
 sudo apt-get update
 sudo apt install cvmfs cvmfs-config
-sudo cvmfs_config setup
 
 sudo cp default.local  /etc/cvmfs/default.local
 sudo cp galaxyproject.org.conf  /etc/cvmfs/domain.d/galaxyproject.org.conf
 sudo cp ./*.galaxyproject.org.pub /etc/cvmfs/keys/
-
-mkdir -p /home/runner/cvmfs/data
-mkdir -p /home/runner/cvmfs/main
-sudo mount -t cvmfs main.galaxyproject.org /home/runner/cvmfs/main
-sudo mount -t cvmfs data.galaxyproject.org /home/runner/cvmfs/data
+sudo mkdir -p /cvmfs/main.galaxyproject.org
+sudo mkdir -p /cvmfs/data.galaxyproject.org
+sudo mount -t cvmfs main.galaxyproject.org /cvmfs/main.galaxyproject.org
+sudo mount -t cvmfs data.galaxyproject.org /cvmfs/data.galaxyproject.org
